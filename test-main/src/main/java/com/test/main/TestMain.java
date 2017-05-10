96ad9708afb5544d5434b857c2068a2ca22e6d08
vtferrari <vtferrari@hotmail.com>
2017-05-10 03:44:25 -0300

    configs de startup

--- test-main/src/main/java/com/test/main/TestMain.java
+++ test-main/src/main/java/com/test/main/TestMain.java
@@ -6,21 +6,28 @@ package com.test.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Start da aplicação
+ *
  * @author esdrastavares
  *
  */
 @SpringBootApplication
 @ComponentScan({
-	"com.test.api"
+    "com.test.api",
+    "com.test.service.api",
+    "com.test.repository.api.user",
 })
+@EntityScan({"com.test.model.api"})
+@EnableJpaRepositories({"com.test.repository.api.user"})
 public class TestMain {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestMain.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestMain.class, args);
+    }
 
 }
