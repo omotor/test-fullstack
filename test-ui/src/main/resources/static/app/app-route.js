e8837bb6e477d9a1bec8e8385c5158fbb9ca7fb1
lricardolisboa <lricardolisboa@gmail.com>
2017-05-12 10:09:50 -0300

    Adicionando Implementação para CRUD do modelo User
    Signed-off-by: lricardolisboa <lricardolisboa@gmail.com>

--- test-ui/src/main/resources/static/app/app-route.js
+++ test-ui/src/main/resources/static/app/app-route.js
@@ -1,12 +1,33 @@
 
 'use strict';
 
-angular.module('test').config(['$stateProvider', function ($stateProvider) {
+angular.module('test')
+.config(['$stateProvider', function ($stateProvider) {
 
-	$stateProvider.state('app', {
-		url: '/home',
-		templateUrl: '/app/app-template.html',
-		controller: 'AppController'
-	});
+	$stateProvider
+		.state('app', {
+			url: '/home',
+			templateUrl: '/app/app-template.html',
+			controller: 'AppController'
+		})
+		.state('users-list', {
+			url: '/user/',
+			templateUrl: '/app/views/user-list.html',
+			controller: 'UserListController',
+			controllerAs: 'userListCtrl'
+		})
+		.state('user-new', {
+			url: '/user/add/:id',
+			templateUrl: '/app/views/user-new.html',
+			controller: 'UserNewController',
+			controllerAs: 'userCtrl'
+		})
+		
+		.state('user-update', {
+			url: '/user/:id',
+			templateUrl: '/app/views/user-update.html',
+			controller: 'UserUpdateController',
+			controllerAs: 'userCtrl'
+		});
 
 }]);
